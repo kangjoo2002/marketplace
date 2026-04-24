5bd9060fd0a24e7a9a691696083f7158fcd0ceb0
joohyeongzz81 <joohyeongzz81@gmail.com>
2026-04-24 16:03:25 +0900

    chore: Spring Boot 애플리케이션 생성

---
+++ src/main/java/com/portfolio/readpath_lab/ReadpathLabApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.readpath_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadpathLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadpathLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/readpath_lab/ReadpathLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.readpath_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadpathLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
